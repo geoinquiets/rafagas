95b4ecc86b4aa92c297cecf9356e601e85b9e692
Jorge Gaspar Sanz Salinas <jorge.sanz@elastic.co>
2024-06-28 16:43:17 +0200

    refactor check url into a its own script

---
+++ script/fix_links.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+import os
+import re
+import random
+import logging
+from multiprocessing import Pool
+from pathlib import Path
+import datetime
+import frontmatter
+
+from utils.check_url import checkUrl
+
+
+# create logger
+for key in logging.Logger.manager.loggerDict:
+    logging.getLogger(key).setLevel(logging.CRITICAL)
+
+for handler in logging.root.handlers[:]:
+    logging.root.removeHandler(handler)
+
+LINKS_PROCESS = int(os.environ.get("LINKS_PROCESS") or 50)
+LOG_LEVEL = os.environ.get("LINKS_LOG_LEVEL") or logging.INFO
+LOG_FORMAT = " %(asctime)s - %(levelname)-8s %(message)s"
+LOG_DATE_FMT = "%I:%M:%S %p"
+
+POOL_SIZE = 15
+CHECK_DAYS = 7
+
+logging.basicConfig(format=LOG_FORMAT, datefmt=LOG_DATE_FMT)
+
+logger = logging.getLogger("fix_links")
+logger.setLevel(LOG_LEVEL)
+logger.info("Logger set up")
+
+
+# Functions
+
+
+def filterInvalid(rafaga):
+    link = rafaga["link"]
+    if "invalid" in rafaga and (
+        rafaga["invalid"] == True or rafaga["invalid"] == "true"
+    ):
+        logger.debug(f"[Invalid] Skipping {link}")
+        return False
+    return True
+
+
+def processRafaga(post, skipInvalids=True):
+    rafagas = (
+        filter(filterInvalid, post["rafagas"]) if skipInvalids else post["rafagas"]
+    )
+
+    for rafaga in rafagas:
+        link = rafaga["link"]
+        now = datetime.datetime.now()
+        lastCheck = (
+            datetime.datetime.fromisoformat(rafaga["lastCheck"])
+            if "lastCheck" in rafaga
+            else now
+        )
+
+        if lastCheck == now or (now - lastCheck).days > CHECK_DAYS:
+            logger.debug(f"Checking {link}")
+            linkCheck = checkUrl(link)
+            rafaga["lastCheck"] = lastCheck.isoformat()
+
+            if linkCheck["code"] >= 400:
+                rafaga["invalid"] = True
+            if linkCheck["url"] != link:
+                rafaga["link"] = linkCheck["url"]
+        else:
+            logger.debug(f"[Checked] Skipping {link}")
+    return post
+
+
+def processFile(md):
+    result = "Not a rafaga"
+    with md.open() as md_reader:
+        post = frontmatter.load(md_reader)
+        if "rid" in post:
+            rid = post["rid"]
+            result = "Read"
+            logger.debug("Processing rafaga {}...".format(rid))
+            post_processed = processRafaga(post)
+            if post_processed is not None:
+                result = "Written"
+                with md.open(mode="w") as md_writer:
+                    md_writer.write(frontmatter.dumps(post_processed))
+                logger.debug("Rafaga %s processed with result %s", rid, result)
+        return {"file": str(md), "result": result}
+
+
+
+
+
+if __name__ == "__main__":
+    # Main
+    p = Path(f"{os.path.dirname(__file__)}/../_posts/")
+
+    allPosts = list(filter(lambda f: str(f).find("template") == -1, p.glob("**/*.md")))
+    logger.info("%s rafagas in the repository", len(allPosts))
+
+    randomizedPosts = list(allPosts)
+    random.shuffle(randomizedPosts)
+
+    posts = randomizedPosts[:LINKS_PROCESS]
+
+    logger.info("Processing %s rafagas", LINKS_PROCESS)
+
+    with Pool(POOL_SIZE) as p:
+        results = p.map(processFile, posts)
+
+    logger.info(f"{len(posts)} rafagas processed")
---
+++ script/utils/check_url.py
@@ -0,0 +1,88 @@
+import requests
+import requests
+import math
+import re
+from requests import Timeout
+import urllib3
+import logging
+import os
+import sys
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# Constants
+LOG_LEVEL = os.environ.get('LINKS_LOG_LEVEL') or logging.INFO
+LOG_FORMAT = ' %(asctime)s - %(levelname)-8s %(message)s'
+LOG_DATE_FMT = '%I:%M:%S %p'
+
+
+MAX_TIMEOUT = 17;
+TIMEOUT = 2;
+
+HEADERS = {
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36',
+    'Accept': 'text/html',
+    'Accept-Language': 'es-ES,es;q=0.9,ca;q=0.8,en-US;q=0.7,en;q=0.6'
+}
+logging.basicConfig(format=LOG_FORMAT, datefmt=LOG_DATE_FMT)
+
+logger = logging.getLogger('check_url')
+logger.setLevel(LOG_LEVEL)
+
+def replaceHttpHttps(url):
+    return re.sub('^http:', 'https:', url)
+
+def checkUrl(url, timeout=TIMEOUT):
+    try:
+        rHead = requests.head(
+            url,
+            headers = HEADERS,
+            allow_redirects = False,
+            timeout = timeout,
+            verify = False
+        )
+        
+        urlR = rHead.headers['Location'] if 'Location' in rHead.headers and rHead.headers['Location'][:4] == 'http' else url
+
+        rGet = requests.get(
+            urlR,
+            headers= HEADERS,
+            timeout = timeout,
+            verify=False
+        )
+        
+        code = 200 if rGet.status_code == 403 and 'Server' in rGet.headers and rGet.headers['Server'].lower() == 'cloudflare' else rGet.status_code
+        
+        return {
+            'url': urlR,
+            'code': code
+        }
+    except Timeout as e:
+        if re.match('^http:', url):
+            logger.debug(f'trying {url} with https')
+            return checkUrl(replaceHttpHttps(url))
+        else:
+            if timeout < MAX_TIMEOUT:
+                new_timeout = math.floor(timeout * 2.0)
+                logger.debug(f'trying {url} increasing the timeout to {new_timeout}')
+                return checkUrl(url, timeout= new_timeout)
+            else:
+                return {
+                    'url': url,
+                    'code': 504
+                }
+    except Exception as e:
+        logger.error(e)
+        return {
+            'url': url,
+            'code': 500
+        }
+
+
+if __name__ == "__main__":
+    try:
+        url = sys.argv[1]
+        check = checkUrl(url, TIMEOUT)
+        logger.info(check)
+    except Exception as e:
+        logger.error(e)
